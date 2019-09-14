f9800d9ad69b544ca16c2b6e7f63e9d281a8acba
SJH <1039460820@qq.com>
2019-09-14 11:06:27 +0800

    add let rem string

--- src/async/async.js
+++ src/async/async.js
@@ -1,21 +1,63 @@
-function tell(){
-  return new Promise((reslove, reject)=>{
-    if(true){
-      reslove(1)
-    } else{
-      reject(2)
-    }
+// function tell(){
+//   return new Promise((reslove, reject)=>{
+//     if(true){
+//       reslove(1)
+//     } else{
+//       reject(2)
+//     }
+//   })
+// }
+
+// var a = (
+//   async() =>{
+//     await tell().then((res)=>{
+//       console.log('res',res);
+//     }).catch(err=>{
+//       console.log('err: ', err);
+//     })
+//   }
+// ) 
+// console.log('a',a())
+
+
+// async function async1(){
+//   const js =  await async2();
+//   console.log(1)
+// }
+
+
+// function async2(){
+//   console.log('async2')
+// }
+
+// async1();
+
+async function doThing() {
+  await doA()
+  await doB()
+}
+
+function doAnotherThing() {
+  console.log('do another thing')
+}
+
+async function doA() {
+  return new Promise(resove => {
+    setTimeout(() => {
+      console.log('done A')
+      resove()
+    }, 0)
   })
 }
 
-var a = (
-  async() =>{
-    await tell().then((res)=>{
-      console.log('res',res);
-    }).catch(err=>{
-      console.log('err: ', err);
-    })
-  }
-) 
-console.log('a',a())
+async function doB() {
+  return new Promise(resove => {
+    setTimeout(() => {
+      console.log('done B')
+      resove()
+    }, 0)
+  })
+}
 
+doThing().then(console.log('done Thing'))
+doAnotherThing()
